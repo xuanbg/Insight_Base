6731e62b89ce5dd528321b4a28ae500faa1135e6
xan.brian <xuan_bingan@hotmail.com>
2015-12-27 17:36:44 +0800

    up

--- Source/Server/Common/Function/DataAccess.cs
+++ Source/Server/Common/Function/DataAccess.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using Insight.WS.Server.Common.ORM;
 using static Insight.WS.Server.Common.SqlHelper;
 
