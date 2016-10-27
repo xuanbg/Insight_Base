44b2bd4211b4581e56fcdd9660bcc617e7f6f7c9
xan.brian <xuan_bingan@hotmail.com>
2016-10-27 14:48:28 +0800

    up

--- Source/OAuth/Properties/AssemblyInfo.cs
+++ Source/OAuth/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("OAuth")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OAuth2")]
+[assembly: AssemblyDescription("OpenAuth公共库")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OAuth")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Insight Software Ltd.")]
+[assembly: AssemblyProduct("Insight Base Service")]
+[assembly: AssemblyCopyright("Copyright © 2014 Insight Software Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
