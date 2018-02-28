77ed56c5b23dda25a76ea77544ec32089e21b4d0
宣炳刚 <xan.brian@gmail.com>
2018-02-28 19:56:27 +0800

    up

--- DataBase/View/RoleMember.sql
+++ DataBase/View/RoleMember.sql
@@ -1,74 +1,33 @@
-﻿USE Insight_Base
-GO
+﻿use insight_base
+go
 
-IF EXISTS (SELECT * FROM sysobjects WHERE id = OBJECT_ID(N'RoleMember') AND OBJECTPROPERTY(id, N'ISVIEW') = 1)
-DROP VIEW RoleMember
-GO
+if exists (select * from sysobjects where id = object_id(N'ucv_role_member') and objectproperty(id, N'isview') = 1)
+drop view ucv_role_member
+go
 
+/*****视图：查询角色成员*****/
 
-/*****视图：查询所有角色的成员*****/
+create view ucv_role_member as
+select
+    m.id, m.role_id, m.member_type, m.member_id, u.name
+from
+	ucr_role_member m
+	join ucb_user u on u.id = m.member_id 
+where
+	m.member_type = 1 union
+select
+    m.id, m.role_id, m.member_type, m.member_id, g.name
+from
+	ucr_role_member m
+	join ucg_group g on g.id = m.member_id 
+where
+	m.member_type = 2 union
+select
+    m.id, m.role_id, m.member_type, m.member_id, o.alias as name
+from
+	ucr_role_member m
+	join uco_organization o on o.id = m.member_id 
+where
+	m.member_type = 3
 
-CREATE VIEW RoleMember
-AS
-
-select distinct '00000000-0000-0000-0000-000000000001' as ID,
-       null as ParentId,
-       RoleId,
-	   '00000000-0000-0000-0000-000000000000' as MemberId,
-       1 as [Index],
-       1 as NodeType,
-       '用户' as Name
-from SYS_Role_Member
-where Type = 1
-union
-select distinct M.ID,
-       cast('00000000-0000-0000-0000-000000000001' as uniqueidentifier) as ParentId,
-       M.RoleId,
-       U.ID as MemberId,
-       U.SN as [Index],
-       0 as NodeType,
-       U.Name
-from SYS_Role_Member M
-join SYS_User U on U.ID = M.MemberId
-union
-select distinct '00000000-0000-0000-0000-000000000002' as ID,
-       null as ParentId,
-       RoleId,
-	   '00000000-0000-0000-0000-000000000000' as MemberId,
-       2 as [Index],
-       2 as NodeType,
-       '用户组' as Name
-from SYS_Role_Member
-where Type = 2
-union
-select distinct M.ID,
-       '00000000-0000-0000-0000-000000000002' as ParentId,
-       M.RoleId,
-       G.ID as MemberId,
-       G.SN as [Index],
-       0 as NodeType,
-       G.Name
-from SYS_Role_Member M
-join SYS_UserGroup G on G.ID = M.MemberId
-union
-select distinct '00000000-0000-0000-0000-000000000003' as ID,
-       null as ParentId,
-       RoleId,
-	   '00000000-0000-0000-0000-000000000000' as MemberId,
-       3 as [Index],
-       3 as NodeType,
-       '职位' as Name
-from SYS_Role_Member
-where Type = 3
-union
-select M.ID,
-       '00000000-0000-0000-0000-000000000003' as ParentId,
-       M.RoleId,
-       P.ID as MemberId,
-       P.SN as [Index],
-       0 as NodeType,
-       case when P.FullName is not null then P.FullName else P.Name end as Name
-from SYS_Role_Member M
-join SYS_Organization P on P.ID = M.MemberId
-
-GO
+go
--- DataBase/View/UserRole.sql
+++ DataBase/View/UserRole.sql
@@ -8,22 +8,26 @@ go
 /*****视图：查询用户拥有的所有角色*****/
 
 create view ucv_user_role as
-select lower(newid()) as id, m.role_id, r.tenant_id, m.member_id as user_id, null as dept_id
+select
+    lower(newid()) as id, m.role_id, r.tenant_id, m.member_id as user_id, null as dept_id
 from ucr_role_member m
-join ucr_role r on r.id = m.role_id
-where m.member_type = 1
-union
-select lower(newid()) as id, m.role_id, r.tenant_id, g.user_id, null as dept_id
+    join ucr_role r on r.id = m.role_id
+where
+    m.member_type = 1 union
+select
+    lower(newid()) as id, m.role_id, r.tenant_id, g.user_id, null as dept_id
 from ucr_role_member m
-join ucr_role r on r.id = m.role_id
-join ucg_group_member g on g.group_id = m.member_id
-where m.member_type = 2
-union
-select lower(newid()) as id, m.role_id, r.tenant_id, o.user_id, d.parent_id as dept_id
+    join ucr_role r on r.id = m.role_id
+    join ucg_group_member g on g.group_id = m.member_id
+where
+    m.member_type = 2 union
+select
+    lower(newid()) as id, m.role_id, r.tenant_id, o.user_id, d.parent_id as dept_id
 from ucr_role_member m
-join ucr_role r on r.id = m.role_id
-join uco_org_member o on o.org_id = m.member_id
-join uco_organization d on d.id = o.org_id
-where m.member_type = 3
+    join ucr_role r on r.id = m.role_id
+    join uco_org_member o on o.org_id = m.member_id
+    join uco_organization d on d.id = o.org_id
+where
+    m.member_type = 3
 
 go
